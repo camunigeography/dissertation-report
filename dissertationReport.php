d988a0f95f1166e37b2ea75b038ac4ed6f3d0c2a
mvl22 <martin@lucas-smith.co.uk>
2022-09-22 23:51:19 +0100

    Migrate fields definition from database to JSON-backed templatisation in sync with https://github.com/camunigeography/reviewable-assessments/commit/2a9b07d498042c5b51a5d01e81afef01ba58bb5e

--- dissertationReport.php
+++ dissertationReport.php
@@ -25,44 +25,24 @@ class dissertationReport extends reviewableAssessments
 	}
 	
 	
-	# Database structure
-	public function databaseStructureSpecificFields ()
-	{
-		# Return the SQL
-		return $sql = "
-			/* Domain-specific fields */
-			
-			`title` mediumtext NOT NULL COMMENT 'Provisional title',
-			`projectDescription` mediumtext NOT NULL COMMENT 'Description',
-			`subject` enum('','Physical Geography', 'Human Geography','Physical and Human areas combined') NOT NULL COMMENT 'Subject area',
-			`supervisors` mediumtext NOT NULL COMMENT 'Supervisors so far',
-			`helpData` enum('','Yes','No') NOT NULL COMMENT 'Help (fieldwork and laboratory techniques)',
-			`helpDataDetails` mediumtext COMMENT 'Help (fieldwork and laboratory techniques) - details',
-			`helpModelling` enum('','Yes','No') NOT NULL COMMENT 'Help (modelling)',
-			`helpModellingDetails` mediumtext COMMENT 'Help (modelling) - details',
-			`helpImagery` enum('','Yes','No') NOT NULL COMMENT 'Help (imagery)',
-			`helpImageryDetails` mediumtext COMMENT 'Help (imagery) - details',
-		";
-	}
-	
 	
 	# Function to define the asssessment form template
 	public function formTemplateLocal ($data, $watermark)
 	{
-		$html = '<h3>Section B &#8211; Dissertation information</h3>';
+		$html  = '<h3>Section B &#8211; Dissertation information</h3>';
 		$html .= $watermark;
 		
 		# Form fields
-		$html .= '
+		$html .= "
 		<br />
 		<h4>A. Provisional dissertation title:</h4>
 		<p>The title of your dissertation should indicate clearly and accurately the nature and topic of your dissertation. This title should be agreed with your Dissertation Supervisor. Subtle changes of wording in the title of the final submitted dissertation, or the addition of a short subtitle which reflects more specifically the nature of the work, will not be penalised by the Part II Examination Board.</p>
-		<p>{title}</p>
+		<p>{title|mediumtext|Provisional title}</p>
 		
 		<br />
 		<h4>B. 100 words describing your dissertation research project:</h4>
 		<p>These will be used to help the Board of Examiners to allocate markers to dissertations, ensuring appropriate expertise.</p>
-		<p>{projectDescription}</p>
+		<p>{projectDescription|mediumtext|Description}</p>
 		
 		<br />
 		<h4>C. Identify a subject area your dissertation topic applies to:</h4>
@@ -71,54 +51,54 @@ class dissertationReport extends reviewableAssessments
 			<li>Human Geography areas (e.g. Urban, Economic, Cultural, Historical Geography etc.)</li>
 			<li>Physical and Human areas combined (e.g. Environmental Management, Environment and Society etc.)</li>
 		</ul>
-		<p>{subject}</p>
+		<p>{subject|enum('','Human Geography','Physical Geography','Human and Physical combined')|Subject area}</p>
 		
 		<br />
 		<h4>D. Name of your Dissertation Supervisor:</h4>
 		<p>If you have seen or are anticipating seeing more than one person (e.g. your original supervisor has left the Department or gone on sabbatical) since the Easter Term 2020 please give both names. Please also list anyone else who your supervisor may have arranged for you to see. Each student is allowed 4 hours of formal dissertation supervision at Part II.</p>
-		<p>{supervisors}</p>
+		<p>{supervisors|mediumtext|Supervisors so far}</p>
 		
 		<br />
 		<h4>E. Technical help - Have you received any of the following (this is in addition to any help from your Dissertation Supervisor):</h4>
 		
-		<table class="graybox questionnaire">
+		<table class=\"graybox questionnaire\">
 			
 			<tr>
 				<td>&hellip; Training or advice in use of online data sets and their analyses?</td>
 				<td>
-					{helpData}<br />
+					{helpData|enum('','Yes','No')|Help (data sets and their analyses)}<br />
 					<p>If Yes give details, e.g. what and from whom:</p>
-					{helpDataDetails}
+					{helpDataDetails|mediumtext|Help (data sets and their analyses) - details}
 				</td>
 			</tr>
 			
 			<tr>
 				<td>&hellip; Training in mathematical modelling or advanced analytical techniques, including computational help?</td>
 				<td>
-					{helpModelling}<br />
+					{helpModelling|enum('','Yes','No')|Help (modelling)}<br />
 					<p>If Yes give details, e.g. what and from whom:</p>
-					{helpModellingDetails}
+					{helpModellingDetails|mediumtext|Help (modelling) - details}
 				</td>
 			</tr>
 			
 			<tr>
 				<td>&hellip; Advice on the availability of remotely sensed imagery, and on the analysis of remotely sensed data using advanced software packages?</td>
 				<td>
-					{helpImagery}<br />
+					{helpImagery|enum('','Yes','No')|Help (imagery)}<br />
 					<p>If Yes give details, e.g. what and from whom:</p>
-					{helpImageryDetails}
+					{helpImageryDetails|mediumtext|Help (imagery) - details}
 				</td>
 			</tr>
 		</table>
-		';
+		";
 		
 		# Final confirmation
-		$html .= '
-			<h3 class="pagebreak">Confirmation</h3>
-			<div class="graybox">
+		$html .= "
+			<h3 class=\"pagebreak\">Confirmation</h3>
+			<div class=\"graybox\">
 				<p><strong>I confirm the above details are correct: {confirmation}</strong></p>
 			</div>
-		';
+		";
 		
 		# Return the HTML
 		return $html;
